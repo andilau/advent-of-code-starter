27376bd85c7164ddd378416f6c2adc60dcbc845a
Andi Lau <andi@herrlau.de>
2022-01-02 18:08:52 +0100

    Upgrade to Reflections 0.10.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,8 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.reflections:reflections:0.9.12")
+    implementation("org.reflections:reflections:0.10.2")
+    implementation("org.slf4j:slf4j-nop:1.7.32")
 
     testApi("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.assertj:assertj-core:3.21.0")
